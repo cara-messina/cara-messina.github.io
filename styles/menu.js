19ea696b8d29ced5c930fb3ec71340d4bc6d405a
Cara Marta Messina <cara.messi@gmail.com>
2025-02-17 12:21:01 -0500

    Adding

---
+++ styles/menu.js
@@ -0,0 +1,36 @@
+document.write('\
+\
+<link rel="stylesheet" href="/styles/menu.css"></link>\
+\
+\<ul class="topnav">\
+\
+<li><a href="/index.html" class="logo">Cara Marta Messina </a></li>\
+\
+  <li><a href="/about/index.html">About Me</a></li>\
+  \  <li><a href="/job_materials/index.html">Job Materials</a></li>\
+ \
+  <li class="dropdown">\
+   <a href="javascript:void(0)" class="dropbtn">Fan Studies</a>\
+  \<div class="dropdown-content">\
+  \<a href="/fan-studies/index.html">About Fan Studies</a>\
+  \<a href="/fan-studies/models/all_ff_topic_model.html">Topic Model: All Fanfics</a>\
+  \</div>\
+  \</li>\
+  \
+   <li class="dropdown">\
+    <a href="javascript:void(1)" class="dropbtn">Teaching</a>\
+   \<div class="dropdown-content">\
+   \<a href="/pedagogy/index.html">Critical Digital Pedagogy</a>\
+   \<a href="/pedagogy/syllabus/writing-for-social-media-syllabus.pdf">Writing for Social Media Syllabus</a>\
+   \<a href="/pedagogy/remiXML/index.html">remiXML: First Year Writing</a>\
+   \</div>\
+   \</li>\
+      \
+  <li><a href="/writing/index.html">Writing</a></li>\
+      \
+  <li><a href="http://www.criticalfantoolkit.org">Critical Fan Toolkit</a></li>\
+      \
+  <li><a href="https://github.com/caramessina">GitHub</a></li>\
+  \
+</ul>\
+')
